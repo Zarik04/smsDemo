37d5d06ee7cd8e7424153b7ecefc9163a15770bd
Zarshedjon Nasimov <2110058@newuzbekistanuniversity.uz>
2023-02-13 01:55:29 +0500

    added view of resource section in lms

--- src/main/java/com/example/smsdemo/HelloApplication.java
+++ src/main/java/com/example/smsdemo/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("course/resource.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
         stage.setTitle("Login Page !");
         stage.setScene(scene);
